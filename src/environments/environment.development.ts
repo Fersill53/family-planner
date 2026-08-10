ca8da40292052373c9b3b3af820fb00f6178ffe4
Fernando Siller <Fersill53@outlook.com>
2026-08-10 08:25:58 -0500

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+production: false,
+
+firebase: {
+  apiKey: "AIzaSyBNgO7j4yNYlshyjSPGpFVD-OceIbdJG2Q",
+  authDomain: "family-scheduler-6c579.firebaseapp.com",
+  projectId: "family-scheduler-6c579",
+  storageBucket: "family-scheduler-6c579.firebasestorage.app",
+  messagingSenderId: "904981420640",
+  appId: "1:904981420640:web:9d656566e78cc617704105"
+}
+
+};
+
